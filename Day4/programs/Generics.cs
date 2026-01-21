f90f1647210ccb24719523873df53dbdd7ea89f8
Devam <satasiyadevam@gmail.com>
2026-01-21 18:18:24 +0530

    update: completed Programs

---
+++ Day4/programs/Generics.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day4.programs
+{
+    internal class DataRepository<T>
+    {
+        
+    }
+    internal class Generics
+    {
+        // 
+        static void Swap<T>(ref T  valuea , ref T valueb)
+        {
+            T temp = valuea;
+            valuea = valueb;
+            valueb = temp;
+        }
+
+
+        static void Main(string[] args)  {
+
+            float valuea = 1232.4f;
+            float valueb = 433.45f;
+            Console.WriteLine("Before Swaping");
+            Console.WriteLine("valuea:"+valuea + " valueb: "+ valueb);
+            Swap<float>(ref valuea, ref valueb);
+            Console.WriteLine("After Swaping");
+            Console.WriteLine("valuea:" + valuea + "valueb: " + valueb);
+            
+        }
+    }
+}
---
+++ Day4/programs/PaymentService/Payment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Day4.programs.Payment
+{
+    internal class Service
+    {
+        public void DisplayServiceName()
+        {
+            Console.WriteLine("This is the Display Service!");
+        }
+    }
+}
+
